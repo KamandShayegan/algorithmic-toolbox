1fdc3346746cf508afc1cc76f4cd565534964d2b
k.shaigan <k.shaigan@narvanstartupstudio.com>
2022-08-26 15:14:03 +0430

    update assignment 1

--- assignments/assignment_1/Main.java
+++ assignments/assignment_1/Main.java
@@ -2,13 +2,16 @@ package assignment_1;
 import java.util.Scanner;
 
 public class Main{
+
+  static int add(int num1, int num2){
+    return num1 + num2;
+  }
     public static void main(String[] args) {
-    Operations op = new Operations();
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
     int firstNum = myObj.nextInt(); //Get users input for the first number
     int secondNum = myObj.nextInt(); //Get users input for the second number
     myObj.close();
-    int res = op.add(firstNum, secondNum);
+    int res = add(firstNum, secondNum);
     System.out.println(res);
   }
 }
--- assignments/assignment_1/Operations.java
+++
@@ -1,7 +0,0 @@
-package assignment_1;
-
-public class Operations{
-    int add(int num1, int num2){
-        return num1 + num2;
-      }
-}
