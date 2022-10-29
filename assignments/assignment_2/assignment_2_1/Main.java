14e487ac3a218320d274eb8a7b9db1e3101a1ec3
Kamand <kamand@shayegan.com>
2022-10-29 13:30:23 +0330

    fix timelimit

--- assignments/assignment_2/assignment_2_1/Main.java
+++ assignments/assignment_2/assignment_2_1/Main.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main{
 
   static int fib(int num){
-  
-    int res = 0;
- int[] list = {0,1};
-    if(num==0) res = list[0];
-    if(num==1) res = list[1];
-    if(num>1){
-      for(int i = 2; i<=num; i++){
-        int temp = list[i-1] + list[i-2];
-        
-        // list.add(temp);
-        // list[i] = temp;
+    // int res = 0;
+    // if(num ==0) res = 0;
+    // if(num == 1) res = 1;
+    // if(num>1){
+    //   res = fib(num-1) + fib(num-2);
+    // }
+    // return res;
+      int res = 0;
+      ArrayList<Integer> list = new ArrayList<Integer>();
+      list.add(0);
+      list.add(1);
+      if(num==0) res = list.indexOf(0);
+      if(num==1) res = list.indexOf(1);
+      if(num>1){
+        for(int i = 2; i<=num; i++){
+          System.out.println(list.indexOf(i-1));
+          System.out.println(" + ");
+          System.out.println(list.indexOf(i-2));
+
+          int temp = list.indexOf(i-1) + list.indexOf(i - 2);    
+        list.add(temp);
       }
       System.out.print(list);
-      res = list[num];
+      res = list.indexOf(num);
     }
     return res;
   }
